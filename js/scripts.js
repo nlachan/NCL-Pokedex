c2e18565d408cfb28d805fc4cec01ab5100279ac
Neil L <141323568+nlachan@users.noreply.github.com>
2023-09-16 23:38:35 -0700

    Basic Start to Pokedex
    basic start creating an array of Pokémon's and their attributes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert("Hello World");
-let favoriteFood = "Chinese Food and Thai Food";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+  {
+    name: "Bulbasaur",
+    type: ["Grass", "Poison"],
+    species: "Seed",
+    height: 0.7,
+  },
+  {
+    name: "Charmander",
+    type: ["Fire"],
+    species: "Lizard",
+    height: 0.6,
+  },
+  {
+    name: "Squirtle",
+    type: ["Water"],
+    species: "Young Turtle",
+    height: 0.5,
+  },
+];
