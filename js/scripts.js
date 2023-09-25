f717735f3fa5676ea32f75684c9f9f7d153394f9
Neil L <141323568+nlachan@users.noreply.github.com>
2023-09-25 00:08:25 -0700

    Updated Tasks
    updated pokemonlist and used document.write() for loop code. Added a conditional within the loop to point out a big pokemon and stated " wow thats a big pokemon"

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,56 @@
-let pokemonList = [];
-pokemonList = [
-  {
-    name: "Bulbasaur",
-    type: ["Grass", "Poison"],
-    species: "Seed",
-    height: 0.7,
-  },
-  {
-    name: "Charmander",
-    type: ["Fire"],
-    species: "Lizard",
-    height: 0.6,
-  },
-  {
-    name: "Squirtle",
-    type: ["Water"],
-    species: "Young Turtle",
-    height: 0.5,
-  },
-];
+const pokemonRepository = (function () {
+  // Array containing Pokemon objects
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      type: ["Grass", "Poison"],
+      species: "Seed",
+      height: 0.7,
+    },
+    {
+      name: "Charizard",
+      type: ["Fire", "Flying"],
+      species: "Lizard",
+      height: 1.7,
+    },
+    {
+      name: "Squirtle",
+      type: ["Water"],
+      species: "Young Turtle",
+      height: 0.5,
+    },
+  ];
+
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    },
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Charmander" });
+console.log(pokemonRepository.getAll());
+
+/* The loop code below was replaced with the foreach() which does the same thing*/
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height >= 1) {
+    document.write(
+      pokemon.name +
+        " " +
+        "Height:" +
+        " " +
+        pokemon.height +
+        " - Wow! That's a big Pokemon! " +
+        "<br>"
+    );
+  } else if (pokemon.height) {
+    document.write(
+      pokemon.name + " " + "Height:" + " " + pokemon.height + "<br>"
+    );
+  }
+});
